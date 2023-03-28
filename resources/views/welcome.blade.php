149e24536cc745294a83d2c96eac55d0c98951ea
Eleonora Leone <eleonora.leone3@yahoo.com>
2023-03-28 21:20:05 +0200

    prova Eleonora

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,4 @@
 <x-main>
-<<<<<<< HEAD
     <div class="container-fluid text-center">
         <x-navbar/>
       <div class="row">
@@ -8,9 +7,5 @@
            
         </div>
       </div>
-=======
-    <div class="container text-center">
-        <h1>Presto & Bene .</h1>
->>>>>>> ee07f8bd0f00ea76e451efb5ccb9ae12f385c3a5
     </div>
 </x-main>
