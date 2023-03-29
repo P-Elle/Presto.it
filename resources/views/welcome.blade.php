6725fdd7c38e646e422cd8ade30f4d877703b7c1
Duccio Stefanelli <duccio.stefanelli@gmail.com>
2023-03-29 20:44:23 +0200

    refactory home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,19 @@
 <x-main>
     <div class="container-fluid text-center">
       <x-navbar/>
-      <div class="row">
-        <div class="masthead px-0">
-          <div class="container h-100">
-          <div class="row h-100 align-content-center justify-content-center">
-              <div class="col-6 text-center">
-                  <h1>Inserisci i tuoi annunci e trova i migliori affari!</h1>
-                  <p>
+         <div class="row">
+            <div class="masthead px-0">
+              <div class="container h-100">
+                <div class="row h-100 align-content-center justify-content-center">
+                  <div class="col-6 text-center">
+                    <h1>Inserisci i tuoi annunci e trova i migliori affari!</h1>
+                    <p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni inventore facere suscipit tenetur. Odit eveniet sequi cupiditate exercitationem incidunt eligendi?
-                  </p>
-              </div> <!--END col-12-->
-          </div> <!--END row-->
-          </div> <!--END container-->
-        </div> <!--END masthead-->
-      </div>
+                    </p>
+                  </div> <!--END col-12-->
+                </div> <!--END row-->
+              </div> <!--END container-->
+            </div> <!--END masthead-->
+          </div>
     </div>
 </x-main>
