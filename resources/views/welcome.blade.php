ebc1d075496a15e208b10cdf70d4229a2c599c92
Duccio Stefanelli <duccio.stefanelli@gmail.com>
2023-03-30 19:45:45 +0200

    welcome e inzio css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <x-main>
     <div class="container-fluid px-0 text-center">
       <x-navbar/>
-<<<<<<< HEAD
          
             <div class="masthead">
               {{-- <div class="container h-100"> --}}
@@ -38,30 +37,5 @@
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dignissimos unde, autem accusantium quis quod voluptatem porro numquam maxime quidem doloremque rerum aspernatur odit saepe.</p>
             </div>
           
-=======
-      {{-- HEADER --}}
-      <div class="row">
-        <div class="masthead px-0">
-          <div class="container h-100">
-            <div class="row h-100 align-content-center justify-content-center">
-              <div class="col-6 text-center">
-                <h1>Inserisci i tuoi annunci e trova i migliori affari!</h1>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni inventore facere suscipit tenetur. Odit eveniet sequi cupiditate exercitationem incidunt eligendi?
-                </p>
-              </div> 
-            </div> 
-          </div> 
-        </div>
-      </div>
-
-      {{-- SEZIONE ANNUNCI --}}
-      <section class="my-5">
-        <div class="container">
-          <div class="row justify-content-center ms-5">
-          </div>
-        </div>
-      </section>
->>>>>>> main
     </div>
 </x-main>
