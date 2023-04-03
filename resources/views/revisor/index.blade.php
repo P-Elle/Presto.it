cad25623476d36850eb43ec12226e6c8fce67f23
mimmo <angiullifx@gmail.com>
2023-04-03 14:59:43 +0200

    modifica home revisor

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,5 +1,5 @@
 <x-main>
-
+    <x-navbar/>
     <div class="container-fluid p-5 bg-gradient bg-success p-5 shadow mb-4">
         <div class="row">
             <div class="col-12 text-light p-5">
@@ -18,14 +18,33 @@
             <div class="col-10 col-md-6 mx-auto justify-content-center ann-detail">
 
                 <div class="row detail brand-white-bg justify-content-center">
+                    {{-- campo descrittivo --}}
                     <div class="col-12 col-lg-5 p-4">
                         <h2 class="brand">{{$announcement_to_check->title}}</h2>
                         <h3>{{$announcement_to_check->price}} €</h3>
                         <p class="small mt-1">{{$announcement_to_check->category->name ?? "categoria non registrata"}}</p>
                         <p>{{$announcement_to_check->description}}</p>
+                        <p>                       
+                            <form action="{{ route('revisor.accept_announcement',['announcement'=>$announcement_to_check]) }}"
+                            method="POST">
+                            @csrf
+                             @method('PATCH')    
+                            <button class="btn btn-success shadow" type="submit">Accetta</button>
+                            </form>
+                        </p>
+                        <p>
+                            <form action="{{ route('revisor.reject_announcement',['announcement'=>$announcement_to_check]) }}"
+                                method="POST">
+                            @csrf
+                            @method('PATCH')    
+                                <button class="btn btn-danger shadow" type="submit">Rifiuta</button>
+                            </form>  
+                        </p>
                     </div>
+
+                    {{-- carosello --}}
                     <div class="col-12 col-lg-7 align-item-center justify-content-center p-4">
-                        {{-- carosello --}}
+                        
                         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
                             <div class="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -60,7 +79,7 @@
             </div>
         </div>
         {{-- BOTTONI REVISORE ACCETTA E RIFIUTA --}}
-        <div class="row">
+        <div class="row  detail brand-white-bg justify-content-center">
             <div class="col-10 col-md-6 mx-auto justify-content-center ann-detail">
 
                 <div class="row detail brand-white-bg justify-content-center">
