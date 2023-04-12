fc343b7c6baf71d441204f54649c06cc8899ba7f
Duccio Stefanelli <duccio.stefanelli@gmail.com>
2023-04-12 17:17:47 +0200

    search

--- resources/views/components/search-announcement.blade.php
+++ resources/views/components/search-announcement.blade.php
@@ -1,13 +1,13 @@
 <div class="row justify-content-center">
-    <div class="col-10 col-md-8 brand-cool-bg p-2">
+    <div class="col-12 col-md-8 brand-cool-bg p-2">
         <form action="{{route('announcements.search')}}" method="GET">
 
             <div class="row justify-content-center">
                 
-                    <div class="col-md-5 p-2 pe-md-0">
+                    <div class="col-md-5 pe-sm-0">
                         <input type="search" name="searched" class="form-control" value="{{old('searched')}}">
                     </div>
-                    <div class="col-md-5 p-2 p-md-0">
+                    <div class="col-md-5">
                         <select class="form-select" name="searchCategory">
                             <option selected value="0">Categorie</option>
                             @foreach ($categories as $category)
@@ -17,7 +17,7 @@
 
                         </select>
                     </div>
-                    <div class="col-md-2 p-2 ps-md-0 d-flex">
+                    <div class="col-md-2 ps-0 d-flex">
                         <button class="btn brand-dark-bg brand-white flex-fill" type="submit">Search</button>
                     </div>
                     
