e5298225a441be1498aa308fd3b0db3e3507e5cf
Duccio Stefanelli <duccio.stefanelli@gmail.com>
2023-04-18 21:42:02 +0200

    drag&drop non funzionante

--- resources/views/livewire/create-announcement2.blade.php
+++ resources/views/livewire/create-announcement2.blade.php
@@ -42,55 +42,20 @@
                         <span class="text-danger small">{{$message}}</span>
                         @enderror
                     </div>
-                    @if (!empty($images))
-                    
-                         <div class="col-12 mt-1 preview-box">
-                             <p>Anteprima immagini:</p>
-                             <div x-data="{images: @entangle('images')}" wire:sortable="updateImageOrder" class="row mx-auto rounded py-4 brand-white-bg" x-init="() => {
-                                const container = document.querySelector('[wire\\:sortable]')
-                                const sortable = new Sortable(container, {
-                                    draggable: '.image-container',
-                                    handle: 'img',
-                                })
-                            }">
-                         
-
-                                 @foreach ($images as $key => $image)
-
-                                         <div class="image-preview mx-auto" wire:sortable.item="{{$key}}" wire:key="image-{{$key}}" wire:sortable.handle style="background-image: url({{$image->temporaryUrl()}});">
-                                             <button type="button" class="btn remove m-0 p-0 border-0" wire:click="removeImage({{$key}})"><i class="bi bi-x-circle-fill"></i></button>
-                                             {{-- <button wire:sortable.handle>drag</button> --}}
-                                         </div>
-                                      
-
-                                     
-                                     
-                                 @endforeach
-                                
-                             </div>
-                        
-                         
-                        </div>
-                     @endif
-                     
-                 
                     <div class="col-12 mt-1">
-                        <label for="temporary_images">Immagini</label>
                         <input wire:model="temporary_images" type="file" name="images" multiple class="form-control">
                         @error('temporary_images.*')
                         <span class="text-danger small">{{$message}}</span>
                         @enderror
-                       {{-- @if (!empty($images))
+                       @if (!empty($images))
                        <div class="row">
-                            <div class="col-12 mt-2">
-                                <p class="mt-2">Photo Preview:</p>
-                                <div class="row border border-4 border-info rounded shadow py-4 brand-white-bg">
+                            <div class="col-12">
+                                <p>Photo Preview:</p>
+                                <div class="row border border-4 border-info rounded shadow py-4">
                                     @foreach ($images as $key => $image)
                                         <div class="col my-3">
-                                            <div class="image-preview mx-auto shadow rounded" style="background-image: url({{$image->temporaryUrl()}}); position:relative;">
-                                                <button type="button" class="btn remove m-0 p-0 border-0" wire:click="removeImage({{$key}})"><i class="bi bi-x-circle-fill"></i></button>
-                                            </div>
-
+                                            <div class="image-preview mx-auto shadow rounded" style="background-image: url({{$image->temporaryUrl()}});"></div>
+                                            <button type="button" class="btn btn-danger shadow d-block text-center mt-2 mx-auto" wire:click="removeImage({{$key}})">Cancella</button>
                                         </div>
                                         
                                     @endforeach
@@ -100,7 +65,7 @@
 
                         @endif
                         
-                    </div> --}}
+                    </div>
                     <div class="col-12 mt-1 mb-3">
                         <button type="submit" class="btn brand-dark-bg brand-white my-3">Crea</button>
                     </div>
