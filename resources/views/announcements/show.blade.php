559ae20e7c86dcc20fd83b921e53da5392389f61
Duccio Stefanelli <duccio.stefanelli@gmail.com>
2023-04-20 21:37:02 +0200

    pre-review generico debug

--- resources/views/announcements/show.blade.php
+++ resources/views/announcements/show.blade.php
@@ -15,33 +15,28 @@
 
                         {{-- carosello --}}
                         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-                            <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <div>
+                            {{-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            </div>
+                            </div> --}}
                             <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="{{!$announcement->images()->get()->isEmpty() ? {{--Storage::url($announcement->images()->first()->path)--}}
-                                $announcement->images()->first()->getUrl(400,400) : 'https://picsum.photos/400/400'}}" class="v-100 d-block"alt="{{$announcement->title}}">
+                                @foreach ($announcement->images as $key => $image)
+                                <div class="carousel-item  @if($loop->first)active @endif">
+                                    <img src="{{
+                                $image->getUrl(400,400)}}" class="v-100 d-block"alt="{{$announcement->title}}">
 
-                            </div>
-                            <div class="carousel-item">
-                                <img src="{{!$announcement->images()->get()->isEmpty() ? {{--Storage::url($announcement->images()->first()->path)--}}
-                                $announcement->images()->first()->getUrl(400,400) : 'https://picsum.photos/400/400'}}" class="v-100 d-block"alt="{{$announcement->title}}">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="{{!$announcement->images()->get()->isEmpty() ? {{--Storage::url($announcement->images()->first()->path)--}}
-                                $announcement->images()->first()->getUrl(400,400) : 'https://picsum.photos/400/400'}}" class="v-100 d-block"alt="{{$announcement->title}}">
-                            </div>
+                                </div>
+                                @endforeach
+                           
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">{{__('ui.previous')}}</span>
+                            <span class="visually-hidden">{{__("ui.previous")}}</span>
                             </button>
                             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">{{__('ui.next')}}</span>
+                            <span class="visually-hidden">{{__("ui.next")}}</span>
                             </button>
                         </div>
                     </div>
--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,28 +1,28 @@
 <div class="row brand-top mb-0 g-3 justify-content-center d-flex">
     <div class="col-12 col-md-6 brand-grey-bg mx-0 px-5 not-overlapping">
-        <h1 class="my-3 text-center">{{__('ui.create')}}</h1>
+        <h1 class="my-3 text-center">Crea il tuo annuncio!</h1>
         @if(session('message'))
             <div class="alert alert-success my-2">{{session('message')}}</div>
         @endif
-
+        
             <form wire:submit.prevent="store">
                 @csrf
                 <div class="col-12 mt-1 mb-3">
-                    <label for="title">{{__('ui.title')}}</label>
+                    <label for="title">Titolo Annuncio</label>
                     <input wire:model="title" type="text" class="form-control @error('title') is-invalid @enderror">
                     @error('title')
                     <span class="text-danger small">{{$message}}</span>
                     @enderror
                 </div>
                 <div class="col-12 mt-1 mb-3">
-                    <label for="description">{{__('ui.description')}}</label>
+                    <label for="description">Descrizione</label>
                     <textarea wire:model="description" type="text" class="form-control @error('description') is-invalid @enderror"></textarea>
                     @error('description')
                     <span class="text-danger small">{{$message}}</span>
                     @enderror
                 </div>
                 <div class="col-12 mt-1 mb-3">
-                    <label for="price">{{__('ui.price')}}</label>
+                    <label for="price">Prezzo</label>
                     <input wire:model="price" type="number" step="0.01"  class="form-control @error('price') is-invalid @enderror">
                     @error('price')
                     <span class="text-danger small">{{$message}}</span>
@@ -31,9 +31,9 @@
                 {{-- elenco categorie disponibili --}}
                 <div class="row d-flex align-items-end">
                     <div class="col-12 mt-1 mb-3">
-                        <label for="category">{{__('ui.cateory')}}</label>
+                        <label for="category">Categoria</label>
                         <select wire:model.defer="category" id="category" class="form-control @error('category') is-invalid @enderror">
-                            <option value="">{{__('ui.choose')}}</option>
+                            <option value="">Scegli la categoria</option>
                             @foreach ($categories as $category)"
                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                             @endforeach
@@ -42,48 +42,71 @@
                         <span class="text-danger small">{{$message}}</span>
                         @enderror
                     </div>
-
                     @if (!empty($images))
-
+                    
                          <div class="col-12 mt-1 preview-box">
-                             <p>{{__('ui.img')}}</p>
-                             <div  wire:sortable="updateImageOrder" class="row mx-auto rounded py-4 brand-white-bg">
+                             <p>Anteprima immagini:</p>
+                             <div x-data="{images: @entangle('images')}" wire:sortable="updateImageOrder" class="row mx-auto rounded py-4 brand-white-bg" x-init="() => {
+                                const container = document.querySelector('[wire\\:sortable]')
+                                const sortable = new Sortable(container, {
+                                    draggable: '.image-container',
+                                    handle: 'img',
+                                })
+                            }">
+                         
 
+                                 @foreach ($images as $key => $image)
 
-
-                            @foreach ($images as $key => $image)
-
-                                         <div class="image-preview mx-auto" draggable="true" ondragstart="event.dataTransfer.setData('text/plain', this.dataset.order); event.dataTransfer.effectAllowed = 'move';">
+                                         <div class="image-preview mx-auto" wire:sortable.item="{{$key}}" wire:key="image-{{$key}}" wire:sortable.handle style="background-image: url({{$image->temporaryUrl()}});">
                                              <button type="button" class="btn remove m-0 p-0 border-0" wire:click="removeImage({{$key}})"><i class="bi bi-x-circle-fill"></i></button>
                                              {{-- <button wire:sortable.handle>drag</button> --}}
                                          </div>
+                                      
 
-
-
-
+                                     
+                                     
                                  @endforeach
-
+                                
                              </div>
-
-
+                        
+                         
                         </div>
                      @endif
-
-
+                     
+                 
                     <div class="col-12 mt-1 mb-3">
-                        <label for="temporary_images">{{__('ui.jpg')}}</label>
+                        <label for="temporary_images">Immagini</label>
                         <input wire:model="temporary_images" type="file" name="images" multiple class="form-control">
                         @error('temporary_images.*')
                         <span class="text-danger small">{{$message}}</span>
                         @enderror
+                       {{-- @if (!empty($images))
+                       <div class="row">
+                            <div class="col-12 mt-2">
+                                <p class="mt-2">Photo Preview:</p>
+                                <div class="row border border-4 border-info rounded shadow py-4 brand-white-bg">
+                                    @foreach ($images as $key => $image)
+                                        <div class="col my-3">
+                                            <div class="image-preview mx-auto shadow rounded" style="background-image: url({{$image->temporaryUrl()}}); position:relative;">
+                                                <button type="button" class="btn remove m-0 p-0 border-0" wire:click="removeImage({{$key}})"><i class="bi bi-x-circle-fill"></i></button>
+                                            </div>
+
+                                        </div>
+                                        
+                                    @endforeach
+                                </div>
+                            </div>
+                        @endif
+                        
+                    </div> --}}
                     <div class="col-12 mt-1 mb-3">
-                        <button type="submit" class="btn brand-dark-bg brand-white my-3">{{__('ui.create2')}}</button>
+                        <button type="submit" class="btn brand-dark-bg brand-white my-3">Crea</button>
                     </div>
                 </div>
             </form>
 
-
-
+       
+        
     </div>
-
+    
 </div>
