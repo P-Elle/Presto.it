34d5caed96972c160f5650097d9ad5bb6d0330c7
Duccio Stefanelli <duccio.stefanelli@gmail.com>
2023-04-22 12:44:55 +0200

    css adattato a nuova bar

--- resources/views/livewire/create-announcement.blade.php
+++ resources/views/livewire/create-announcement.blade.php
@@ -1,28 +1,28 @@
 <div class="row brand-top mb-0 g-3 justify-content-center d-flex">
-    <div class="col-12 col-md-6 brand-grey-bg mx-0 px-5 not-overlapping">
-        <h1 class="my-3 text-center">{{__('ui.create')}}</h1>
+    <div class="col-12 col-md-6 brand-grey-bg mx-0 mt-5 px-5 not-overlapping create">
+        <h1 class="my-3 text-center">Crea il tuo annuncio!</h1>
         @if(session('message'))
             <div class="alert alert-success my-2">{{session('message')}}</div>
         @endif
-
+        
             <form wire:submit.prevent="store">
                 @csrf
                 <div class="col-12 mt-1 mb-3">
-                    <label for="title">{{__('ui.title')}}</label>
+                    <label for="title">Titolo Annuncio</label>
                     <input wire:model="title" type="text" class="form-control @error('title') is-invalid @enderror">
                     @error('title')
                     <span class="text-danger small">{{$message}}</span>
                     @enderror
                 </div>
                 <div class="col-12 mt-1 mb-3">
-                    <label for="description">{{__('ui.description')}}</label>
+                    <label for="description">Descrizione</label>
                     <textarea wire:model="description" type="text" class="form-control @error('description') is-invalid @enderror"></textarea>
                     @error('description')
                     <span class="text-danger small">{{$message}}</span>
                     @enderror
                 </div>
                 <div class="col-12 mt-1 mb-3">
-                    <label for="price">{{__('ui.price')}}</label>
+                    <label for="price">Prezzo</label>
                     <input wire:model="price" type="number" step="0.01"  class="form-control @error('price') is-invalid @enderror">
                     @error('price')
                     <span class="text-danger small">{{$message}}</span>
@@ -31,9 +31,9 @@
                 {{-- elenco categorie disponibili --}}
                 <div class="row d-flex align-items-end">
                     <div class="col-12 mt-1 mb-3">
-                        <label for="category">{{__('ui.category')}}</label>
+                        <label for="category">Categoria</label>
                         <select wire:model.defer="category" id="category" class="form-control @error('category') is-invalid @enderror">
-                            <option value="">{{__('ui.choose')}}</option>
+                            <option value="">Scegli la categoria</option>
                             @foreach ($categories as $category)"
                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                             @endforeach
@@ -43,9 +43,9 @@
                         @enderror
                     </div>
                     @if (!empty($images))
-
+                    
                          <div class="col-12 mt-1 preview-box">
-                             <p>{{__('ui.preview')}}</p>
+                             <p>Anteprima immagini:</p>
                              <div x-data="{images: @entangle('images')}" wire:sortable="updateImageOrder" class="row mx-auto rounded py-4 brand-white-bg" x-init="() => {
                                 const container = document.querySelector('[wire\\:sortable]')
                                 const sortable = new Sortable(container, {
@@ -53,7 +53,7 @@
                                     handle: 'img',
                                 })
                             }">
-
+                         
 
                                  @foreach ($images as $key => $image)
 
@@ -61,22 +61,22 @@
                                              <button type="button" class="btn remove m-0 p-0 border-0" wire:click="removeImage({{$key}})"><i class="bi bi-x-circle-fill"></i></button>
                                              {{-- <button wire:sortable.handle>drag</button> --}}
                                          </div>
+                                      
 
-
-
-
+                                     
+                                     
                                  @endforeach
-
+                                
                              </div>
-
-
+                        
+                         
                         </div>
                      @endif
-
-
+                     
+                 
                     <div class="col-12 mt-1 mb-3">
-                        <label for="temporary_images">{{__('ui.jpg')}}</label>
-                        <input wire:model="temporary_images" type="file" name="images" multiple class="form-control">
+                        <label for="temporary_images">Immagini</label>
+                        <input wire:model="temporary_images" type="file" id="temporary_images" name="images" multiple class="form-control">
                         @error('temporary_images.*')
                         <span class="text-danger small">{{$message}}</span>
                         @enderror
@@ -92,21 +92,21 @@
                                             </div>
 
                                         </div>
-
+                                        
                                     @endforeach
                                 </div>
                             </div>
                         @endif
-
+                        
                     </div> --}}
                     <div class="col-12 mt-1 mb-3">
-                        <button type="submit" class="btn brand-dark-bg brand-white my-3">{{__('ui.create2')}}</button>
+                        <button type="submit" class="btn brand-dark-bg brand-white my-3">Crea</button>
                     </div>
                 </div>
             </form>
 
-
-
+       
+        
     </div>
-
+    
 </div>
