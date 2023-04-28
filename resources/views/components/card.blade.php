7cf55aff7fc414692950f7d16551cde4e2e309a8
ANTONIO BASTA <antoniobasta@gmail.com>
2023-04-28 21:13:38 +0200

    bug fix linguaggio

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,8 @@
 <div class="col-12 col-lg-4 col-md-6 d-flex justify-content-center" data-aos="flip-up">
     <div class="card brand-alpha px-0 my-4 rounded-0" style="width: 15rem;">
         <div class="c-header">
-            <img src="{{!$announcement->images()->get()->isEmpty() ? {{--Storage::url($announcement->images()->first()->path)--}} 
-            $announcement->images()->first()->watermark() : 'https://picsum.photos/800/800'}}" class="img-fluid w-100" alt="foto-annuncio"> 
+            <img src="{{!$announcement->images()->get()->isEmpty() ? {{--Storage::url($announcement->images()->first()->path)--}}
+            $announcement->images()->first()->watermark() : $announcement->image}}" class="img-fluid w-100" alt="foto-annuncio">
             <span class="btn rounded-1 c-category brand-grey brand-trasp-cool-bg"><a href="{{route('category.view', $announcement->category)}}">
                 @if (App::isLocale('it'))
                     {{($announcement->category->name)}}
@@ -11,12 +11,12 @@
                 @elseif (App::isLocale('es'))
                     {{($announcement->category->name_es)}}
                 @endif
-                
+
             </a></span>
 
         </div>
         <div class="card-body z-ind brand-grey-bg ps-0">
-            
+
         </div>
         <div class="card-body-text  brand-cool-bg ps-0">
             <h4><a class="link-announcement brand-grey" href="{{route('announcements.show', $announcement)}}">{{$announcement->title}}</a></h4>
@@ -29,10 +29,10 @@
                 @elseif (App::isLocale('es'))
                     {{($announcement->category->name_es)}}
                 @endif
-                
+
                 </a>
         </div>
-       
+
         {{-- <div class="card-footer small p-2">
             <div class="row">
                 <div class="col-6 text-center text-secondary">{{$announcement->user->name}}</div>
